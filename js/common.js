814f3d30f7b64355a2bb7ce09a03da8e59e9b4a0
Serhii <serhii@Marinas-MacBook-Air.local>
2022-08-04 23:57:05 +0300

    first commit

---
+++ js/common.js
@@ -0,0 +1,27 @@
+// Check browser support WEBP
+
+function isWebp() {
+
+    function testWebP(callback) {
+        
+
+    let webP = new Image();
+    webP.onload = webP.onerror = function() {
+        callback(webP.height == 2);
+    };
+    webP.src =  'data:image/webp;base64,UklGRi4AAABXRUJQVlA4ICIAAABQAQCdASoDAAIAAgA2JQBOgC6gAP73M8eLuxHGTv3eIAAA'
+    console.log(webP.src);
+}
+
+// add className
+testWebP(function (support) {
+    let className = support === true ? 'webp' : 'no-webp';
+    document.documentElement.classList.add(className);
+});
+
+}
+
+isWebp();
+
+
+
